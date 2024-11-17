9712e57ef190d4cba7e5ce2cc2b8e8650555b101
airmaso <itslawmason@gmail.com>
2024-11-17 14:16:05 -0600

    added home presenter

---
+++ src/main/java/interface_adapters/home/HomePresenter.java
@@ -0,0 +1,59 @@
+package interface_adapters.home;
+
+import interface_adapters.ViewManagerModel;
+import interface_adapters.create.CreateViewModel;
+import interface_adapters.edit.EditViewModel;
+import interface_adapters.study.StudyViewModel;
+import use_cases.home.HomeOutputBoundary;
+import use_cases.home.HomeOutputData;
+
+
+/**
+ * The presenter for our home use case.
+ */
+public class HomePresenter implements HomeOutputBoundary {
+    private final HomeViewModel homeViewModel;
+    private final CreateViewModel createViewModel;
+    private final EditViewModel editViewModel;
+    private final StudyViewModel studyViewModel;
+    private final ViewManagerModel viewManagerModel;
+
+    public HomePresenter(ViewManagerModel vmm, HomeViewModel hvm,
+                         CreateViewModel cvm, EditViewModel evm, StudyViewModel svm) {
+        viewManagerModel = vmm;
+        homeViewModel = hvm;
+        createViewModel = cvm;
+        editViewModel = evm;
+        studyViewModel = svm;
+    }
+
+    @Override
+    public void prepareSuccessView(HomeOutputData homeOutputData) {
+        final HomeState homeState = homeViewModel.getState();
+
+        homeState.updateIDs(homeOutputData.getIDs());
+        homeState.updateTitles(homeOutputData.getTitles());
+        homeState.updateDescriptions(homeOutputData.getDescriptions());
+        homeState.updateFronts(homeOutputData.getFronts());
+        homeState.updateBacks(homeOutputData.getBacks());
+
+        homeViewModel.setState(homeState);
+        homeViewModel.firePropertyChanged();
+    }
+
+    @Override
+    public void switchToCreateView() {
+        viewManagerModel.setState(createViewModel.getViewName());
+        viewManagerModel.firePropertyChanged();
+    }
+
+    @Override
+    public void switchToEditView(String cardSetTitle) {
+        // TODO
+    }
+
+    @Override
+    public void switchToStudyView(String cardSetTitle) {
+        // TODO
+    }
+}
